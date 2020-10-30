b2592521ce5b8b8e2b10d1b6930fab4177ff8913
Ronen Bhaumik <ronenbhaumik@yahoo.com>
2020-10-30 11:42:49 +1100

    Sync trial

--- src/main/java/ghost/Fruit.java
+++ src/main/java/ghost/Fruit.java
@@ -14,6 +14,7 @@ public class Fruit extends GameCell {
         }
     }
 
+    // Determines if fruit object has been consumed by Waka
     public boolean isEaten(Waka waka) {
         if (waka.x == this.x && waka.y == this.y) {
             System.out.println("Eaten!");
