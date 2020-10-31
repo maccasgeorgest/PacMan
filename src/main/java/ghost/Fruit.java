c415f5f569a2b5844c956441dc8c915e1d1d321f
Ronen Bhaumik <ronenbhaumik@yahoo.com>
2020-11-01 02:07:18 +1100

    random fruit trials

--- src/main/java/ghost/Fruit.java
+++ src/main/java/ghost/Fruit.java
@@ -13,7 +13,7 @@ public class Fruit extends GameCell {
 
     public void tick(App app) {
         this.isEaten(app.waka); 
-        if (!this.uneaten) {
+        if (this.uneaten == false) {
             this.sprite = new PImage();
             if (!this.accounted) {
                 app.fruitCount--;
@@ -25,24 +25,35 @@ public class Fruit extends GameCell {
     // Determines if fruit object has been consumed by Waka
     public void isEaten(Waka waka) {
         if (this.uneaten) { 
-            // TOP
-            if (waka.Bottom() < this.Bottom() && waka.Bottom() > this.Top()
-                && waka.Left() >= this.Left() && waka.Right() <= this.Right()) {
-                this.uneaten = false;
-            }
-            // BOTTOM
-            else if (waka.Top() < this.Bottom() && waka.Top() > this.Top()
-                && waka.Left() >= this.Left() && waka.Right() <= this.Right()) {
-                this.uneaten = false;
-            }
-            // LEFT
-            else if (waka.Right() < this.Right() && waka.Right() > this.Left()
-                && waka.Top() >= this.Top() && waka.Bottom() <= this.Bottom()) {
-                this.uneaten = false;
-            }
-            // RIGHT
-            else if (waka.Left() < this.Right() && waka.Left() > this.Left()
-                && waka.Top() >= this.Top() && waka.Bottom() <= this.Bottom()) {
+            // // TOP
+            // if (waka.Bottom() <= this.Bottom() && waka.Bottom() >= this.Top()
+            //     && waka.Left() >= this.Left() && waka.Right() <= this.Right()) {
+            //     this.uneaten = false;
+            // }
+            // // BOTTOM
+            // else if (waka.Top() <= this.Bottom() && waka.Top() >= this.Top()
+            //     && waka.Left() >= this.Left() && waka.Right() <= this.Right()) {
+            //     this.uneaten = false;
+            // }
+            // // LEFT
+            // else if (waka.Right() <= this.Right() && waka.Right() >= this.Left()
+            //     && waka.Top() >= this.Top() && waka.Bottom() <= this.Bottom()) {
+            //     this.uneaten = false;
+            // }
+            // // RIGHT
+            // else if (waka.Left() <= this.Right() && waka.Left() >= this.Left()
+            //     && waka.Top() >= this.Top() && waka.Bottom() <= this.Bottom()) {
+            //     this.uneaten = false;
+            // }
+
+            // if (waka.Left() < this.Right() &&
+            //     waka.Right() > this.Left() &&
+            //     waka.Top() < this.Bottom() &&
+            //     waka.Bottom() > this.Top()) {
+            //         this.uneaten = false;
+            // }
+
+            if (this.Left() == waka.Left() && this.Top() == waka.Top()) {
                 this.uneaten = false;
             }
         }
