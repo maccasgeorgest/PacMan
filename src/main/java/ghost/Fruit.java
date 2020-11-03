842382b9502e1f3cb1e298926dc28b58f8f7f272
Ronen Bhaumik <ronenbhaumik@yahoo.com>
2020-11-03 18:48:53 +1100

    update

--- src/main/java/ghost/Fruit.java
+++ src/main/java/ghost/Fruit.java
@@ -25,35 +25,24 @@ public class Fruit extends GameCell {
     // Determines if fruit object has been consumed by Waka
     public void isEaten(Waka waka) {
         if (this.uneaten) { 
-            // // TOP
-            // if (waka.Bottom() <= this.Bottom() && waka.Bottom() >= this.Top()
-            //     && waka.Left() >= this.Left() && waka.Right() <= this.Right()) {
-            //     this.uneaten = false;
-            // }
-            // // BOTTOM
-            // else if (waka.Top() <= this.Bottom() && waka.Top() >= this.Top()
-            //     && waka.Left() >= this.Left() && waka.Right() <= this.Right()) {
-            //     this.uneaten = false;
-            // }
-            // // LEFT
-            // else if (waka.Right() <= this.Right() && waka.Right() >= this.Left()
-            //     && waka.Top() >= this.Top() && waka.Bottom() <= this.Bottom()) {
-            //     this.uneaten = false;
-            // }
-            // // RIGHT
-            // else if (waka.Left() <= this.Right() && waka.Left() >= this.Left()
-            //     && waka.Top() >= this.Top() && waka.Bottom() <= this.Bottom()) {
-            //     this.uneaten = false;
-            // }
-
-            // if (waka.Left() < this.Right() &&
-            //     waka.Right() > this.Left() &&
-            //     waka.Top() < this.Bottom() &&
-            //     waka.Bottom() > this.Top()) {
-            //         this.uneaten = false;
-            // }
-
-            if (this.Left() == waka.Left() && this.Top() == waka.Top()) {
+            // HIT TOP
+            if (waka.Bottom() <= this.Bottom() && waka.Bottom() >= this.Top()
+                && waka.Left() >= this.Left() && waka.Right() <= this.Right()) {
+                this.uneaten = false;
+            }
+            // HITBOTTOM
+            else if (waka.Top() <= this.Bottom() && waka.Top() >= this.Top()
+                && waka.Left() >= this.Left() && waka.Right() <= this.Right()) {
+                this.uneaten = false;
+            }
+            // LEFT
+            else if (waka.Right() <= this.Right() && waka.Right() >= this.Left()
+                && waka.Top() >= this.Top() && waka.Bottom() <= this.Bottom()) {
+                this.uneaten = false;
+            }
+            // RIGHT
+            else if (waka.Left() <= this.Right() && waka.Left() >= this.Left()
+                && waka.Top() >= this.Top() && waka.Bottom() <= this.Bottom()) {
                 this.uneaten = false;
             }
         }
