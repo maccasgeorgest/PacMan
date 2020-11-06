73c9a940f276c9019ac29e2f3fa2436118932cbf
Ronen Bhaumik <ronenbhaumik@yahoo.com>
2020-11-06 15:51:15 +1100

    superfruit added

---
+++ src/main/java/ghost/Superfruit.java
@@ -0,0 +1,9 @@
+package ghost;
+
+import processing.core.PImage;
+
+public class Superfruit extends Fruit {
+    public Superfruit(PImage sprite, int x, int y) {
+        super(sprite, x, y);
+    }
+}
